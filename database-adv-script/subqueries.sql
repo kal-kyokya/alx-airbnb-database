c12a148a3b39105e5473b120758877a4e5698c90
kal-kyokya <kalkyokya4@gmail.com>
2025-05-15 10:44:01 +0000

    Write both correlated and non-correlated subqueries.

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,19 @@
+-- SQL Script handling both correlated and non-correlated subqueries
+
+SELECT *
+FROM Property
+WHERE property_id IN (
+      SELECT property_id, AVG(rating) AS avg_rating
+      FROM Review
+      GROUP BY property_id
+      HAVING AVG(rating) > 4
+);
+
+SELECT *
+FROM User
+WHERE user_id IN (
+      SELECT user_id, COUNT(booking_id) AS bookings
+      FROM Booking
+      GROUP BY user_id
+      HAVING COUNT(booking_id) > 3
+);
