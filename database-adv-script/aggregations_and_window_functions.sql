02a3e261d235db9619bd314329d9b482f82c87a0
kal-kyokya <kalkyokya4@gmail.com>
2025-05-15 11:40:24 +0000

    Use SQL aggregation and window function to analyze data.

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,9 @@
+-- SQL Script using aggregation and window functions to analyze data
+
+SELECT user_id, COUNT(booking_id) AS bookings
+FROM Booking
+GROUP BY user_id;
+
+SELECT *,
+RANK() OVER (PARTITION BY property_id ORDER BY booking_id DESC) AS ranking
+FROM Booking;
